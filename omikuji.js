bc9aae3fe1a8719cbfa70b81c282b3689c4c4fb1
ROMKT1408 <119I211@stu.nit.ac.jp>
2022-10-10 19:57:52 +0900

    1st commit

--- omikuji.js
+++ omikuji.js
@@ -1,7 +1,7 @@
 let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
-if (!username){
+if (username == ""){
     username="名無し"
 }
 document.getElementById("name").innerHTML = username;
